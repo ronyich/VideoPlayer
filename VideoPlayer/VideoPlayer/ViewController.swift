83297ac48828a50621a701ec10d50a86c471920b
ronyich <ronyich0401@gmail.com>
2019-01-16 11:07:22 +0800

    VideoPlayer project init.

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Ron Yi on 2019/1/16.
+//  Copyright © 2019 Ron Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
